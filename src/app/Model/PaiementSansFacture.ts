5ad57b8ffc309e98b9e6d4ee7bd7366c3a30d271
nourbouslama <nourbouslama410@gmail.com>
2022-06-15 12:01:38 +0100

    15-06 commit

---
+++ src/app/Model/PaiementSansFacture.ts
@@ -0,0 +1,10 @@
+import { Client } from "./Client";
+import { Paiement } from "./Paiement";
+
+export class PaiementSansFacture extends Paiement{
+
+
+    cli?:Client;
+
+
+}
---
+++ src/app/service/paiement.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Facture } from '../Model/Facture';
+import { Paiement } from '../Model/Paiement';
+import { PaiementAvecFacture } from '../Model/PaiementAvecFacture';
+import { PaiementSansFacture } from '../Model/PaiementSansFacture';
+import { AuthentifierService } from './connection.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaiementService {
+
+  apiURL: string = 'http://localhost:8080/caisses';
+  constructor(private http: HttpClient, private authService: AuthentifierService) {
+  }
+ 
+
+  listerPaiements(numS: number): Observable<Paiement[]>{
+    return this.http.get<Paiement[]>(this.apiURL+'/paiement/listerPaiement/'+numS);
+    }
+
+  ListerTousPaiements(): Observable<Paiement[]>{
+    return this.http.get<Paiement[]>(this.apiURL+'/paiement/ListerTousPaiements');
+    }
+
+  ajouterPaiement( paiement: PaiementAvecFacture):Observable<PaiementAvecFacture>{
+    return this.http.post<PaiementAvecFacture>(this.apiURL+'/paiementAvecFacture/ajouterPaiement', paiement);
+    }
+
+  saisirAvance( paiement: PaiementSansFacture):Observable<PaiementSansFacture>{
+    return this.http.post<PaiementSansFacture>(this.apiURL+'/paiementSansFacture/saisirAvance', paiement);
+    }
+
+  PayerFacture(factures : Facture[]) {
+    const url = `${this.apiURL}/paiementAvecFacture/payer`;
+    return this.http.put(url, factures);
+    }
+
+  AnnulerPaiement(factures : Facture[]):Observable<PaiementAvecFacture> {
+    const url = `${this.apiURL}/paiementAvecFacture/annuler`;
+    return this.http.put<PaiementAvecFacture>(url, factures);
+    }
+
+  modifierPaiements(paiements: PaiementAvecFacture[]) {
+    return this.http.put("http://localhost:8095/synch/paiement/modifierPaiements",paiements);
+    }
+
+}
